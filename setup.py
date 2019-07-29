37b380a19a8ba28d16e46132fdc706772d6b80e4
Eduard Carreras <ecarreras@gisce.net>
2019-07-29 13:38:44 +0200

    Bump to v0.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kais-export',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(),
     url='https://github.com/gisce/kais-export',
     license='GPLv2',
